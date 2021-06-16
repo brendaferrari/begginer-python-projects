0e6d8c9f09b2dd4a17bae5ae4734d7c81786fa11
brendaferrari <brendafferrari@gmail.com>
2021-06-16 13:49:50 -0300

    simple madlibs

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+adj = input("Adjective: ")
+noun1 = input("Noun: ")
+noun2 = input("Noun: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+youtuber = input("Youtuber: ")
+
+madlib = f"Today I woke up {adj}. I really wanted to {noun1} more but my {noun2} didn't let me. I took a shower and had my breakfast \
+really fast because I was late to work. After work I {verb1} because I love to {verb2} and then I watched {youtuber} before sleeping. "
+
+print(madlib)
